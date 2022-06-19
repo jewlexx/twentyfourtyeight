c30584b21fb67bc72ea31938f4976d3608060756
Juliette Cordor <grassojames5@gmail.com>
2022-06-19 14:10:10 +1000

    basic board implementation

---
+++ src/board.rs
@@ -0,0 +1,155 @@
+use thiserror::Error as AsError;
+
+type Result<T> = std::result::Result<T, BoardError>;
+
+#[derive(Debug, AsError, PartialEq, Eq)]
+pub enum BoardError {
+    #[error("Must be between {min} and {max}. Got {value}")]
+    RangeError {
+        min: usize,
+        max: usize,
+        value: usize,
+    },
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Cell {
+    Filled(u128),
+    Empty,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Board {
+    pub cells: [Cell; 9],
+}
+
+impl Board {
+    pub fn get_row(&self, row: usize) -> Result<[Cell; 3]> {
+        if !(1..=3).contains(&row) {
+            Err(BoardError::RangeError {
+                min: 1,
+                max: 3,
+                value: row,
+            })
+        } else {
+            // Forces the array to be sized
+            let row = self.cells[(row - 1) * 3..row * 3].to_vec();
+            Ok([row[0], row[1], row[2]])
+        }
+    }
+
+    pub fn get_column(&self, column: usize) -> Result<[Cell; 3]> {
+        if !(1..=3).contains(&column) {
+            Err(BoardError::RangeError {
+                min: 1,
+                max: 3,
+                value: column,
+            })
+        } else {
+            Ok([
+                self.cells[column - 1],
+                self.cells[column + 2],
+                self.cells[column + 5],
+            ])
+        }
+    }
+
+    pub fn get_cell(&self, row: usize, column: usize) -> Result<Cell> {
+        if !(1..=3).contains(&row) {
+            Err(BoardError::RangeError {
+                min: 1,
+                max: 3,
+                value: row,
+            })
+        } else if !(1..=3).contains(&column) {
+            Err(BoardError::RangeError {
+                min: 1,
+                max: 3,
+                value: column,
+            })
+        } else {
+            Ok(self.cells[(row - 1) * 3 + column - 1])
+        }
+    }
+}
+
+mod tests {
+    use super::*;
+
+    static BOARD: Board = Board {
+        cells: [
+            Cell::Filled(1),
+            Cell::Filled(2),
+            Cell::Filled(3),
+            Cell::Filled(4),
+            Cell::Filled(5),
+            Cell::Filled(6),
+            Cell::Filled(7),
+            Cell::Filled(8),
+            Cell::Filled(9),
+        ],
+    };
+
+    #[test]
+    fn test_get_row() {
+        assert_eq!(
+            BOARD.get_row(1),
+            Ok([Cell::Filled(1), Cell::Filled(2), Cell::Filled(3)])
+        );
+        assert_eq!(
+            BOARD.get_row(2),
+            Ok([Cell::Filled(4), Cell::Filled(5), Cell::Filled(6)])
+        );
+        assert_eq!(
+            BOARD.get_row(3),
+            Ok([Cell::Filled(7), Cell::Filled(8), Cell::Filled(9)])
+        );
+        assert_eq!(
+            BOARD.get_row(4),
+            Err(BoardError::RangeError {
+                min: 1,
+                max: 3,
+                value: 4,
+            })
+        );
+    }
+
+    #[test]
+    fn test_get_column() {
+        assert_eq!(
+            BOARD.get_column(1),
+            Ok([Cell::Filled(1), Cell::Filled(4), Cell::Filled(7)])
+        );
+        assert_eq!(
+            BOARD.get_column(2),
+            Ok([Cell::Filled(2), Cell::Filled(5), Cell::Filled(8)])
+        );
+        assert_eq!(
+            BOARD.get_column(3),
+            Ok([Cell::Filled(3), Cell::Filled(6), Cell::Filled(9)])
+        );
+        assert_eq!(
+            BOARD.get_column(4),
+            Err(BoardError::RangeError {
+                min: 1,
+                max: 3,
+                value: 4,
+            })
+        );
+    }
+
+    #[test]
+    fn test_get_cell() {
+        assert_eq!(BOARD.get_cell(1, 1), Ok(Cell::Filled(1)));
+        assert_eq!(BOARD.get_cell(2, 2), Ok(Cell::Filled(5)));
+        assert_eq!(BOARD.get_cell(3, 3), Ok(Cell::Filled(9)));
+        assert_eq!(
+            BOARD.get_cell(4, 4),
+            Err(BoardError::RangeError {
+                min: 1,
+                max: 3,
+                value: 4,
+            })
+        );
+    }
+}
