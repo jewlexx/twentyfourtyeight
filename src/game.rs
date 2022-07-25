e7864bc755386f2bd7c5502120b6204b91044bc5
Juliette Cordor <grassojames5@gmail.com>
2022-07-25 19:15:50 +1000

    removed console deps

--- src/game.rs
+++ src/game.rs
@@ -1,19 +1,4 @@
-use std::{error::Error, io};
-
 use bevy::prelude::*;
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{Alignment, Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::Span,
-    widgets::{Block, BorderType, Borders, Widget},
-    Frame, Terminal,
-};
 
 pub mod errors;
 
@@ -33,42 +18,11 @@ impl Game {
         Ok(Game { board })
     }
 
-    pub fn display(&self) -> Result<()> {
-        enable_raw_mode()?;
-
-        let mut stdout = std::io::stdout();
-
-        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-        let backend = CrosstermBackend::new(stdout);
-
-        let mut terminal = Terminal::new(backend)?;
-
-        disable_raw_mode()?;
-
-        execute!(
-            terminal.backend_mut(),
-            LeaveAlternateScreen,
-            DisableMouseCapture
-        )?;
-        terminal.show_cursor()?;
-
-        Ok(())
-    }
-
     pub fn get_board(&self) -> &Board {
         &self.board
     }
 
     pub fn get_board_mut(&mut self) -> &mut Board {
         &mut self.board
-    }
-
-    fn ui<B: Backend>(&self, f: &mut Frame<B>) -> Result<()> {
-        let layout = Layout::default().direction(Direction::Horizontal);
-
-        let block = Block::default();
-        let span = Span::from("Hello, world!");
-
-        Ok(())
     }
 }
